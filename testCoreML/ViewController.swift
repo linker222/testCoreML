2004b72c1cd732bbbc59c1e40e4bb71a98ccc7e8
shing <shing.linker@gmail.com>
2017-12-04 09:19:55 +0800

    Initial Commit

---
+++ testCoreML/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testCoreML
+//
+//  Created by 施宏政 on 2017/12/4.
+//  Copyright © 2017年 施宏政. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
